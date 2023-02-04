1b93c17a0b85ce25dac5273de4bb4b7a14a9bd4f
Shubham Srivastava <smjackson94@gmail.com>
2023-02-04 22:08:41 +0530

    authentication config done

--- utils/auth.js
+++ utils/auth.js
@@ -1,8 +1,16 @@
 let conf = require("../configs/app.conf");
 let send = require("../configs/response.conf");
+var jwt = require('jsonwebtoken');
 let console = conf.console;
 
-exports.isAuthenticated = function(req, res, cb){
-	console.log(JSON.stringify(req.query), JSON.stringify(req.body));
-	cb(null, req);
+exports.isAuthenticated = function (req, res, cb) {
+	let auth = req.headers.authorization;
+	if (auth.indexOf("Bearer ") == 0) {
+		auth = auth.substring(7);
+	}
+	auth = auth.trim();
+	if (!jwt.verify(auth))
+		return send.unauthorized(res, "token expired");
+	req.user = jwt.decode(auth);
+	cb(null, req, res);
 }
